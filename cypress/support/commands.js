78fe5b48d60a4763140a08f1dcdd3ca14d902302
Naveen <snaveen2112@gmail.com>
2025-05-01 11:02:13 +0530

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,31 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
-
-
 
 
 
@@ -46,63 +18,80 @@ Cypress.Commands.add('loginToMedicines', (email, password) => {
   cy.wait(2000);
 });
 
-Cypress.Commands.add('navigateToCompanies', () => {
+Cypress.Commands.add('navigateToCompanies', () => {  //navigateToCompanies
   cy.get('a[href="/emc/browse-companies"]', { timeout: 10000 })
     .should('be.visible')
     .then(($el) => cy.visit($el.prop('href')));
   cy.url().should('include', '/emc/browse-companies');
 });
 
-Cypress.Commands.add('extractCompanyData', () => {
-  cy.get('[class="browse-results"] > a').then(($companies) => {
-    const companyData = [];
-    const indexes = [0, 2, $companies.length - 1];
 
-    indexes.forEach((index) => {
-      const company = $companies[index];
-      const companyName = company.innerText.trim().replace(/\s+/g, '_');
+//llp2
+Cypress.Commands.add('extractCompaniesFromA_B_Z', () => {
+  const targetLetters = ['A', 'B', 'Z'];
+  const companyData = [];
 
-      cy.wrap(company).invoke('attr', 'href').then((relativeUrl) => {
-        cy.visit(`https://www.medicines.org.uk${relativeUrl}`, { timeout: 10000 });
+  cy.get('.browse-menu a.emc-link').each(($link, index) => {
+    const letter = $link.text().trim();
 
-        cy.get('[class="company-details-contact-items"]').then(($contactSection) => {
-          const contactDetails = {};
-          $contactSection.find('div.company-contacts-item-title').each((_, el) => {
-            const label = el.innerText.trim();
-            const value = el.nextElementSibling?.innerText.trim() || '';
+    if (targetLetters.includes(letter)) {
+      cy.wrap($link).invoke('attr', 'href').then((relativeUrl) => {
+        cy.visit(`https://www.medicines.org.uk${relativeUrl}`);
 
-            if (label.includes('Address')) contactDetails.address = value;
-            else if (label.includes('Telephone')) contactDetails.telephone = value;
-            else if (label.includes('Fax')) contactDetails.fax = value;
-            else if (label.includes('Medical Information e-mail')) contactDetails.medicalEmail = value;
-            else if (label.includes('Medical Information Direct Line')) contactDetails.medicalDirectLine = value;
-            else if (label.includes('Out of Hours Telephone')) contactDetails.outOfHoursTelephone = value;
-          });
+        cy.get('[class="browse-results"] > a').then(($companies) => {
+          const total = $companies.length;
+          const indexes = [0, 2, total - 1];
 
-          cy.get('img[alt="Company image"]').invoke('attr', 'src').then((logoRelativeUrl) => {
-            const logoUrl = `https://www.medicines.org.uk${logoRelativeUrl}`;
-            const logoFilename = `${companyName}_logo.png`;
-            const logoPath = `cypress/fixtures/logos1/${logoFilename}`;
+          indexes.forEach((i) => {
+            if (i >= total) return; // skip if index out of range
+            const company = $companies[i];
+            const companyName = company.innerText.trim().replace(/\s+/g, '_');
+            const companyUrl = company.getAttribute('href');
 
-            cy.request({ url: logoUrl, encoding: 'binary' }).then((response) => {
-              expect(response.status).to.eq(200);
-              cy.task('saveLogo', { filePath: logoPath, data: response.body }).then(() => {
-                companyData.push({ company: companyName, contact: contactDetails, logo: logoFilename });
+            // Separate processing after navigation to avoid cy.click() error
+            cy.visit(`https://www.medicines.org.uk${companyUrl}`);
+
+            cy.get('[class="company-details-contact-items"]').then(($contactSection) => {
+              const contactDetails = {};
+              $contactSection.find('div.company-contacts-item-title').each((_, el) => {
+                const label = el.innerText.trim();
+                const value = el.nextElementSibling?.innerText.trim() || '';
+
+                if (label.includes('Address')) contactDetails.address = value;
+                else if (label.includes('Telephone')) contactDetails.telephone = value;
+                else if (label.includes('Fax')) contactDetails.fax = value;
+                else if (label.includes('Medical Information e-mail')) contactDetails.medicalEmail = value;
+                else if (label.includes('Medical Information Direct Line')) contactDetails.medicalDirectLine = value;
+                else if (label.includes('Out of Hours Telephone')) contactDetails.outOfHoursTelephone = value;
+              });
+
+              cy.get('img[alt="Company image"]').invoke('attr', 'src').then((logoRelativeUrl) => {
+                const logoUrl = `https://www.medicines.org.uk${logoRelativeUrl}`;
+                const logoFilename = `${companyName}_logo.png`;
+                const logoPath = `cypress/fixtures/logos/${logoFilename}`;
+
+                cy.request({ url: logoUrl, encoding: 'binary' }).then((response) => {
+                  expect(response.status).to.eq(200);
+                  cy.task('saveLogo', { filePath: logoPath, data: response.body }).then(() => {
+                    companyData.push({ company: companyName, contact: contactDetails, logo: logoFilename });
+                  });
+                });
               });
             });
           });
+
+          // After processing companies in current letter, go back to A-Z list
+          cy.visit('https://www.medicines.org.uk/emc/browse-companies');
         });
       });
-    });
+    }
+  });
 
-    cy.then(() => cy.writeFile('cypress/fixtures/company-contacts1.json', companyData));
+  cy.then(() => {
+    cy.writeFile('cypress/fixtures/company-contacts.json', companyData);
   });
 });
 
-// Cypress.Commands.add('logoutFromMedicines', () => {
-//   cy.get(':nth-child(3)[class="custom-icon expand-icon"]').click({ force: true });
-//   cy.contains('.custom-dropdown-menu .custom-anchor-text-content', 'Log Out').should('be.visible').click({ force: true });
-// });
 Cypress.Commands.add('assertPageUrl', (expectedUrlPart) => {
   cy.url().should('include', expectedUrlPart);
 });
@@ -113,3 +102,11 @@ Cypress.Commands.add('logoutFromMedicines', () => {
     .should('be.visible')
     .click({ force: true });
 });
+
+
+
+
+
+
+
+
